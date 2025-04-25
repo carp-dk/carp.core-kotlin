09acff0783feae5a724d97929721c7d1ab94cbf0
Nigel Sjolin Grech <nigelgrech.ng@gmail.com>
2025-04-25 18:48:52 +0200

    feat: Add YAML parsing support for Workflow definitions and update dependencies

--- carp.analytics.core/src/commonMain/kotlin/dk/cachet/carp/analytics/domain/workflow/Step.kt
+++ carp.analytics.core/src/commonMain/kotlin/dk/cachet/carp/analytics/domain/workflow/Step.kt
@@ -3,10 +3,14 @@ package dk.cachet.carp.analytics.domain.workflow
 import dk.cachet.carp.analytics.domain.data.InputData
 import dk.cachet.carp.analytics.domain.data.OutputData
 import dk.cachet.carp.analytics.domain.process.Process
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 /**
  * Represents a single step in a workflow.
  */
+@SerialName("step")
+@Serializable
 data class Step(
     override val name: String,
     override val description: String,
